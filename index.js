d2c58d968f6652b2fae4e80c1e30f7718a7dd625
Kahlil <kahliljmdavis@gmail.com>
2022-03-02 12:47:11 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+console.log(customerName)
+
+// function upperCaseCustomerName() {
+//     var upperCaseCustomerName = 'BOB';
+//     console.log(upperCaseCustomerName)
+// }
+
+// upperCaseCustomerName()
+
+function upperCaseCustomerName(){
+    customerName = customerName.toLocaleUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "billy"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "not billy"
+}
+
